c748ea5f01345c0fb843287e620c08f49181ccb7
Philipp Steinke <phil.steinke@gmx.de>
2017-06-07 14:15:21 +0200

    asd

--- src/FatPack/Comet.java
+++ src/FatPack/Comet.java
@@ -4,5 +4,11 @@ import java.util.Observable;
 
 public class Comet extends Observable{
 	
+	public Comet() {
+		
+	}
+	
+	//ColorManager colorManager im konstruktor des Comet Managers oder 
+	//wer auch immer eine instanz drüber kommt
 	//if(in flappys x range) setChanged & notify observer
 }
--- src/FatPack/StarValues.java
+++ src/FatPack/StarValues.java
@@ -3,7 +3,7 @@ package FatPack;
 import java.awt.Color;
 
 public class StarValues {
-	public static int INITIAL_COUNT = 75;
+	public static int INITIAL_COUNT = 100;
 	public static int MAX_COUNT = INITIAL_COUNT;
 
 	public static int INITIAL_SPAWN_TOLERANCE_X = 20;
