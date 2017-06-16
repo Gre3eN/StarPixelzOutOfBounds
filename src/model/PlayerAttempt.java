667a79b4786b8482a9c6a6adcc87ff0778f69918
Tim Fock <tim.fock@googlemail.com>
2017-06-16 17:20:57 +0200

    javadoc

--- src/model/PlayerAttempt.java
+++ src/model/PlayerAttempt.java
@@ -1,10 +1,26 @@
 package model;
 
+/**
+ * The PlayerAttempt class returns the name and score
+ * of its objects. It is also used to compare two
+ * PlayerAttempt objects for sorting purposes.   
+ * 
+ * @see Comparable
+ * 
+ * @author timfock
+ */
 public class PlayerAttempt implements Comparable<PlayerAttempt> {
 	
 	private int score;
 	private String name;
 
+	/**
+	 * Creates a new PlayerAttempt object with a name 
+	 * and a score.
+	 * 
+	 * @param name the name of the player
+	 * @param score the score of the player
+	 */
 	public PlayerAttempt (String name, int score) {
 		this.score = score;
 		this.name = name;
@@ -17,7 +33,15 @@ public class PlayerAttempt implements Comparable<PlayerAttempt> {
 	public String getName() {
 		return name;
 	}
-
+	
+	/**
+	 * Compares the current object to another PlayerAttempt object.<br>
+	 * Initially based on their scores and then based on their names<br>
+	 * in case the score is equal.
+	 * 
+	 * @param p PlayerAttempt object that you want to compare to the current object
+	 * @see java.util.Collections#sort(java.util.List)
+	 */
 	@Override
 	public int compareTo(PlayerAttempt p) {
 		if (score > p.score)
